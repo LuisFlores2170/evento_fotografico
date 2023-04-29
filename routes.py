77dd4ebb187afdbcf24ea8fe6bc23e029ca9829d
LuisFlores2170 <hinojosafloresluisfernando@gmail.com>
2023-04-29 09:45:29 -0400

    update_struc

--- routes.py
+++ routes.py
@@ -6,5 +6,9 @@ from main import app
 
 @app.route('/')
 def login():
-    return render_template('login.html')
+    return render_template(
+        'login.html', 
+        foto = "https://cama165802507.files.wordpress.com/2019/09/el-mejor-objetivo-para-fotografia-de-producto-de-tu-tienda-online.jpg"
+        
+    )
     
